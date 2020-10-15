0ca2a8d6ade1db21c14eaffad39c27cf8298e83e
bourgeoa <alain.bourgeois10@gmail.com>
2020-10-15 19:33:32 +0200

    add parameters

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
 # linkRename
-rename recursively solid server links from solid.community to solid.community.net
+Rename recursively solid server links from `solid.community` to `solid.community.net`
 
-files are filtered on extension : .acl, .meta, .ttl
+Files are filtered on extension : `.acl`, `.meta`, `.ttl`
 
-selected links are enclosed in <> and begin with 'https://'
+Selected links are enclosed in `<>` and begin with `https://`.
 
-in each selected link the server part is replaced
+In each selected link the replacement occur from the end of the `server` part.
 
-example :
+## Usage
+From the folder containing `linkRename.js`
 
-linkRename <command> [<folder>]
+```javascript
+./linkRename.js <command> [<parameter>] [ext=[<extension>]] [<folder>]
 
-command being : test, run, help
+// examples : 
+./linkRename.js run ../server/data
+./linkRename.js test ext=acl --folder ../server/data
+```
+
+- <command\> being : `test`, `run`, `help`
+
+- <extension\> : default all (`acl`, `meta` and `ttl`) , else only one
+
+- <parameter\> (level of reporting) : `--file` (default),  `--folder` or `--no`
+
+## Nota :
+The app should be `run` twice. All files containing `solid.community` strings are listed 
+
+Only links may be converted.
+
+On second pass, all remaining files containing `solid.community` and not converted (literal, ...) are listed.
--- linkRename.js
+++ linkRename.js
@@ -4,32 +4,43 @@ const path = require('path'), fs=require('fs')
 
 const source = 'solid.community'
 const target = 'solidcommunity.net'
-const ext = ['acl', 'meta', 'ttl']
-const pattExt = ext.map(ext => new RegExp(`\.${ext}$`))
-let count = [0, 0, 0]
-console.log(pattExt)
-const args = process.argv
+const exts = ['acl', 'meta', 'ttl']
 const commands = ['run', 'test', 'help']
+const params = ['--no', '--folder', '--file']
 
 // usage represents the help guide
 const usage = function() {
   const usageText = `
-  index.js rename links in acl files.
+  linkRename.js rename links in text/turtle files filtered by extension.
 
   usage:
-    index.js <command> <folder>
+    ./linkRename.js <command> [ext=[<extension>]] [<parameter>] [<folder>]
 
-    commands can be:
+    <command> can be :
 
     run:      used to run rename "${source}" to "${target}"
     test:     used to make a blank test
     help:     used to print the usage guide
+
+    <extension> can be : (example ext=acl)
+    acl, meta or ttl   only one, default is all
+
+    <parameter> can be (optional) :
+
+    --file    default, used to list all files to rename
+    --folder  list first level folder where a rename may occur
+    --no      do not list anything
+
+    <folder> : where to begin recursive rename (server, pod, any podFolder)
   `
 
   console.log(usageText)
 }
 
 // test arguments
+
+// test commands
+const args = process.argv.filter(item => !(item.startsWith('--') || item.startsWith('ext=')))
 if (!(args.length === 3 || args.length === 4)) {
   console.log(`incorrect number of arguments`)
   usage()
@@ -42,6 +53,31 @@ if (commands.indexOf(command) == -1) {
   return
 }
 
+// test parameterss
+const parms = process.argv.filter(item => item.startsWith('--'))
+let param = parms[0]
+if (!param) param ='--file'
+if (params.indexOf(param) == -1) {
+  console.log('invalid parameter passed')
+  usage()
+  return
+}
+
+// test extensions
+let ext = process.argv.filter(item => item.startsWith('ext='))
+if (ext && ext[0]) {
+  ext[0] = ext[0].replace('ext=', '')
+  if (exts.indexOf(ext[0]) == -1) {
+    console.log('invalid extension passed')
+    usage()
+    return
+  }  
+}
+else ext = exts
+console.log('extention ' + ext)
+const pattExt = ext.map(ext => new RegExp(`\.${ext}$`))
+console.log(pattExt)
+
 // main
 switch(command) {
   case 'help':
@@ -49,27 +85,42 @@ switch(command) {
     break
   case 'test':
   case 'run':
+    // test folder
     if (!args[3]) { console.log('path is missing'); break }
     if (args[3].endsWith('/')) { console.log('folder shall not end with "/"'); break }
     const pathToServer = args[3]
+    let count = [0, 0, 0]
+    let title = ''
+    console.log('on folder : ' + pathToServer)
 
+    // rename for each extension
     for (const i in pattExt) {
       console.log('\n' + command + ' extension .' + ext[i])
-      console.log('on folder : ' + pathToServer + '\n')
       fromDir(pathToServer,pattExt[i],function(filename) {
         const content = fs.readFileSync(filename).toString()
         const patt = new RegExp(source)
         if (patt.test(content)) {
           const newContent = rename(content, source, target) //content.replace(new RegExp(source, 'g'), target)
-          console.log(filename.split(pathToServer)[1])
+          if (param !== '--no') {
+            const name = filename.split(pathToServer)[1]
+            const test = name.split('/')
+            if (test.length > 1) {
+              if (title !== test[1]) {
+                title = test[1]
+                console.log('  ' + title)
+              }
+            }
+            if (param === '--file') console.log('   ' + name.split(title)[1])
+          }
           count[i] += 1
           if (command === 'run') fs.writeFileSync(filename, newContent)
         }
       })
     }
     for (const i in pattExt) {
-      console.log(`\nFound ${count[i]} "${ext[i]}" with links to rename`)
+      console.log(`\nFound ${count[i]} "${ext[i]}" with strings to rename`)
     }
+    console.log('\n"All strings may not be renamed. All links are renamed"')
   break
 default:
   console.log('invalid command passed')
@@ -105,8 +156,8 @@ function rename (content, source, target) {
     
     if (link[0].startsWith('https://')) {
       let linkSplit = link[0].split('/')
-      let pod = linkSplit[2]
-      linkSplit[2] = pod.replace(source, target)
+      let podName = linkSplit[2]
+      linkSplit[2] = podName.replace(new RegExp(`${source}$`), target)
       link[0] = linkSplit.join('/')
     }
     sourceArray[i] = link.join('>')
